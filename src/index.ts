3784e11e87847d9945936742e42f1d1e59078cdc
mfrydrychowicz <mamgofry@gmail.com>
2021-01-28 17:53:12 +0100

    [#2] Full projekt setup

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import '/styles/App.scss'
+import '/styles/App.scss';
 
 window.addEventListener('load', () => {
-    const header = document.createElement('h1')
-    header.innerText = 'Chess game web app'
+    const header = document.createElement('h1');
+    header.innerText = 'Chess game web app';
 
-    const body = document.querySelector('body')
-    body.appendChild(header)
-})
+    const body = document.querySelector('body');
+    body.appendChild(header);
+});

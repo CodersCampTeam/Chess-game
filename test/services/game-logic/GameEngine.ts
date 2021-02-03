c5d12390c2bab6614008e13491ce95f8f6fc6c1e
mfrydrychowicz <mamgofry@gmail.com>
2021-02-03 19:35:38 +0100

    test updated

--- test/services/game-logic/GameEngine.ts
+++ test/services/game-logic/GameEngine.ts
@@ -7,12 +7,13 @@ describe('Testing GameEngine.getLegalMoves() while playing Pawn', () => {
         const gameEngine = new GameEngine();
         const pawn = gameEngine.board.getPiece({ row: 1, column: 4 });
         const targetSquare = new Square(5, 4);
+        const expected = [new Square(6, 3), new Square(6, 5)];
 
         // Move pawn to enemy row and get moves
         pawn.move(targetSquare);
         const legalMoves = gameEngine.getLegalMoves(targetSquare);
 
         // Test
-        expect(legalMoves.length).toBe(2);
+        expect(legalMoves).toBe(expected);
     });
 });

2f0e5723cef08c998fdb019e39c8e9d15f7f2ecb
lilareliga <lilareliga@gmail.com>
2021-02-04 19:33:16 +0100

    modification after 1st code review

--- src/services/game-logic/GameEngine.ts
+++ src/services/game-logic/GameEngine.ts
@@ -13,16 +13,23 @@ class GameEngine {
     getLegalMoves = (square: Square): Square[] => {
         const piece = this.board.getPiece(square);
         let pieceMoves = piece?.getPossibleMoves() ?? [];
-        return pieceMoves.filter((square) => this.isOccupiedBySameColorPiece(square, piece));
+        return pieceMoves
+            .filter(this.isOnBoard)
+            .filter((square) => !this.isPawnAndBlocked(square, piece))
+            .filter((square) => !this.isOccupiedBySameColor(square, piece));
     };
 
-    private isOccupiedBySameColorPiece = (square: Square, piece: Piece | null): Boolean => {
-        if (piece?.name === PieceNames.PAWN) {
-            return !this.board.getPiece(square);
-        } else {
-            return this.board.getPiece(square)?.color !== piece?.color;
-        }
+    private isOnBoard = (square: Square): Boolean => {
+        // here or in knight.ts (for now knights can get outside the board)
+        return square.row >= 0 && square.row < 8 && square.column >= 0 && square.column < 8;
     };
+
+    private isPawnAndBlocked = (square: Square, piece: Piece | null): Boolean => {
+        return piece?.name === PieceNames.PAWN ? !!this.board.getPiece(square) : false;
+    };
+
+    private isOccupiedBySameColor = (square: Square, piece: Piece | null): Boolean =>
+        this.board.getPiece(square)?.color === piece?.color;
 }
 
 export { GameEngine };

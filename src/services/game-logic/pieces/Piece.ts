0a66e42a17db457a06a6fd0fb29e45aea53309e6
mfrydrychowicz <mamgofry@gmail.com>
2021-02-13 21:16:12 +0100

    [#38] En Passant (#45)

--- src/services/game-logic/pieces/Piece.ts
+++ src/services/game-logic/pieces/Piece.ts
@@ -19,6 +19,12 @@ abstract class Piece {
         this.hasMoved = true;
     }
 
+    getMoveDirection(): number {
+        // Assume that white plays on bottom
+        // row = 0, column = 0 means left upper corner
+        return this.color === Colors.WHITE ? -1 : 1;
+    }
+
     abstract getPossibleMoves(board: Board): Square[];
 }
 

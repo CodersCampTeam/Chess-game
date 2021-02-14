dffa4473c8c555ebbd20bf0c1e948c7a5c977eeb
mfrydrychowicz <mamgofry@gmail.com>
2021-02-14 12:26:04 +0100

    [#60] sounds in ctor POC (#61)

--- src/services/game-logic/Sound.ts
+++ src/services/game-logic/Sound.ts
@@ -1,10 +1,19 @@
 class Sound {
     private lastPlayedMoveSound = 0;
     private AMOUNT_OF_NORMAL_MOVE_SOUNDS = 3;
+    private capturingMoveSound: HTMLAudioElement;
+    private normalMoveSound: HTMLAudioElement[] = [];
+
+    constructor() {
+        this.capturingMoveSound = new Audio('/static/sounds/capture.mp3');
+
+        for (let soundIdx = 0; soundIdx < this.AMOUNT_OF_NORMAL_MOVE_SOUNDS; ++soundIdx) {
+            this.normalMoveSound.push(new Audio(`/static/sounds/${soundIdx}.mp3`));
+        }
+    }
 
     public playCapturingMoveSound(): void {
-        const sound = new Audio('/static/sounds/capture.mp3');
-        sound.play();
+        this.capturingMoveSound.play();
     }
 
     public playNormalMoveSound(): void {
@@ -15,8 +24,7 @@ class Sound {
         while (this.lastPlayedMoveSound === randomNumber) {
             randomNumber = generateRandomNumber();
         }
-        const sound = new Audio(`/static/sounds/${randomNumber}.mp3`);
-        sound.play();
+        this.normalMoveSound[randomNumber].play();
         this.lastPlayedMoveSound = randomNumber;
     }
 }

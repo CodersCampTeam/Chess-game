a09dfa5a9ff0662c61ff00d040ba5a50fac9c4ae
mfrydrychowicz <mamgofry@gmail.com>
2021-02-14 22:04:44 +0100

    [#73] Settings compatibility

--- src/views/OpeningView.ts
+++ src/views/OpeningView.ts
@@ -1,3 +1,5 @@
+import { settings } from '../services/game-logic/Settings';
+
 class OpeningView {
     element: HTMLDivElement;
     constructor(private saveSettings: (soundOn: boolean) => void) {
@@ -108,6 +110,10 @@ class OpeningView {
 
         this.element.classList.add('modal--closed');
         this.saveSettings(sounds.value === 'on');
+
+        settings.layout = layout?.value;
+        settings.pieces = pieces?.value;
+        settings.sounds = sounds.value;
     }
 
     openModal(): void {
